8506c6ee4674f87cdcceac0634380c2d12d6586b
OBrandon <brandonobas@vagelos-ve506-0033.apn.wlan.upenn.edu>
2019-03-29 16:57:14 -0400

    front-end for logging in and signing up

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Confidely</h1>
+          <p>Record it, report it, don’t support it</p>
+        </div>
+      </div>
+    );
+  }
+}
