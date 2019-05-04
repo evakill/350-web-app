54853127f5d6c136d0c067377bab129d4799d606
Josh <jsdoman97@gmail.com>
2019-05-04 14:04:57 -0400

    setting schoolID as cookie for login, signup, and removing on logout

--- src/containers/Logout.js
+++ src/containers/Logout.js
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 import { Redirect } from 'react-router';
 import 'whatwg-fetch';
 import "./Signup.css";
-import {
-  getFromStorage,
-} from '../utils/storage';
+
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 export default class Logout extends Component  {
   constructor(props) {
     super(props);
   }
 
-render() {
+  render() {
+    cookies.remove('schoolID', { path: '/' });
     return (
         <Redirect to='/' />
       );
